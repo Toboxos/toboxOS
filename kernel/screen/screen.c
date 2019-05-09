efbdaf5118dad28ed04014393f7ad134f5e6636b
Tobias Bungard <tobi-bungard@t-online.de>
2019-05-09 13:36:12 +0200

    Fixed wrong varaibles used for calculating screen positions

--- kernel/screen/screen.c
+++ kernel/screen/screen.c
@@ -5,16 +5,16 @@ void printc(uint8_t x, uint8_t y, char c) {
 }
 
 void printc_color(uint8_t x, uint8_t y, char c, uint8_t colorForeground, uint8_t colorBackground, uint8_t blink) {
-	vga_data[y * vga_height + x].character = c;
-	vga_data[y * vga_height + x].attribute.colorForeground = colorForeground;
-	vga_data[y * vga_height + x].attribute.colorBackground = colorBackground;
-	vga_data[y * vga_height + x].attribute.blink = blink;
+	vga_data[y * vga_width + x].character = c;
+	vga_data[y * vga_width + x].attribute.colorForeground = colorForeground;
+	vga_data[y * vga_width + x].attribute.colorBackground = colorBackground;
+	vga_data[y * vga_width + x].attribute.blink = blink;
 }
 
 void clearscreen() {
 	for( int y = 0; y < vga_height; ++y ) {
 		for( int x = 0; x < vga_width; ++x ) {
-			printc(y, x, ' ');
+			printc(x, y, ' ');
 		}
 	}
 }
